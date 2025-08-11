a1eed0b46cafbf181a9a35910801ea605ffd041e
theamarverma <amarverma.lnct@gmail.com>
2025-08-11 10:25:55 +0530

    highcharts

---
+++ src/app/highcharts/_components/Variwide.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import "highcharts/css/highcharts.css";
+
+// Register variwide in v12 (import only)
+import "highcharts/modules/variwide";
+
+export default function Variwide() {
+  const options: Highcharts.Options = {
+    chart: { type: "variwide" },
+    title: { text: "Labor Costs in Europe, 2023" },
+    subtitle: {
+      text:
+        'Source: <a href="http://ec.europa.eu/eurostat/web/' +
+        'labour-market/labour-costs/main-tables">eurostat</a>',
+    },
+    xAxis: { type: "category" },
+    caption: { text: "Column widths are proportional to GDP" },
+    legend: { enabled: false },
+    series: [
+      {
+        type: "variwide",
+        name: "Labor Costs",
+        data: [
+          ["Norway", 51.9, 448716],
+          ["Denmark", 48.1, 376430],
+          ["Belgium", 47.1, 584699],
+          ["Netherlands", 43.3, 1034086],
+          ["France", 42.2, 2822455],
+          ["Germany", 41.3, 4122210],
+          ["Austria", 40.9, 478190],
+          ["Ireland", 40.2, 504620],
+          ["Sweden", 38.9, 540651],
+          ["Finland", 37.1, 274880],
+          ["Italy", 29.8, 2085375],
+          ["Spain", 24.6, 1461889],
+          ["Czech Republic", 18.0, 317387],
+          ["Portugal", 17.0, 265525],
+          ["Greece", 15.7, 220303],
+          ["Poland", 14.5, 750801],
+          ["Romania", 11.0, 324578],
+        ],
+        dataLabels: { enabled: true, format: "€{point.y:.0f}" },
+        tooltip: {
+          pointFormat:
+            "Labor Costs: <b>€ {point.y}/h</b><br>GDP: <b>€ {point.z} million</b><br>",
+        },
+        borderRadius: 3,
+        colorByPoint: true,
+      },
+    ],
+    credits: { enabled: false },
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+}
---
+++ src/app/highcharts/page.tsx
@@ -0,0 +1,15 @@
+import { HightChartDemo } from "./_components/HighChartDemo";
+import Variwide from "./_components/Variwide";
+
+export default function HighChartPage() {
+  return (
+    <>
+      <div className="h-[500px]">
+        <HightChartDemo />
+        <Variwide />
+      </div>
+
+      <div className="bg-red-800 text-3xl">Hello</div>
+    </>
+  );
+}
